e34abd4bd44f009ce57f633567c857d853fe0ba7
cotizacion-rsmautomotriz <sologithub67@gmail.com>
2025-01-31 10:21:28 -0300

    Delete proyecto_cotizacion directory

--- proyecto_cotizacion/app/static/js/main.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    // Ejemplo de código JavaScript
-    const form = document.querySelector('form');
-    if (form) {
-        form.addEventListener('submit', (e) => {
-            // Puedes agregar validaciones o efectos aquí
-        });
-    }
-});
